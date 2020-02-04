aefa23fda3eddc2af3a40c5953ba80267d90995a
Alex Kang <alexhkang4@gmail.com>
2020-02-03 20:16:04 -0700

    Almost done with Flappy

---
+++ Assets/MovePipe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePipe : MonoBehaviour {
+
+    public float speed = 0.4f;
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+        if ()
+    }
+
+    public void Stop() {
+        transform.position = transform.position;
+    }
+}
---
+++ Assets/PipeGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeGenerator : MonoBehaviour {
+
+    public float maxTime = 1.5f;
+    private float timer = 0;
+    public GameObject pipe;
+    // Start is called before the first frame update
+    void Start() {
+        GameObject newPipe = Instantiate(pipe);
+        newPipe.transform.position = transform.position + new Vector3(0, Random.Range(- 0.4f, 0.4f), 0);
+        Destroy(newPipe, 10);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (timer > maxTime) {
+            GameObject newPipe = Instantiate(pipe);
+            newPipe.transform.position = transform.position + new Vector3(0, Random.Range(- 0.4f, 0.4f), 0);
+            Destroy(newPipe, 10);
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
